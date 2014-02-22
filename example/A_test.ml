b532ae5da01e8fe196be0a013c5ee76b34671607
Kasper <kasper.janssens.android@gmail.com>
2014-02-22 10:17:03 +0100

    Got it compiling with ounit

--- example/A_test.ml
+++ example/A_test.ml
@@ -6,4 +6,4 @@ let test () = Pervasives.print_string "koekoek"
 let suite = "A_test">:::[
   "mock_test" >:: test;]
 
-"ocaml" >::: [suite;]
+let _ = run_test_tt_main suite
---
+++ example/compile.sh
@@ -0,0 +1,1 @@
+ocamlfind ocamlc B.ml A.ml A_test.ml -package oUnit -linkpkg -o A_test
